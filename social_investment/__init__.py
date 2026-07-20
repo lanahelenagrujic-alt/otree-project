2a91ede9cefa6740009b8d00efccff77061f66dc
lanahelenagrujic-alt <lana.helena.grujic@gmail.com>
2026-07-20 21:55:35 +0200

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- social_investment/__init__.py
+++ social_investment/__init__.py
@@ -72,7 +72,7 @@ class Group(BaseGroup):
 # Player holds participant-specific state. (It stores each player's investment choice and advisor-following count)
 class Player(BasePlayer):
     gender = models.StringField(
-        choices=['Male', 'Female'],
+        choices=['Moški', 'Ženski'],
         widget=widgets.RadioSelect,
         label='Specify Gender Please?'
         ) 
